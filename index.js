9e93bced7c922e919ef2fdc55dedc30acdd753c7
Lovro <hodak.lovro@gmail.com>
2020-12-08 22:32:37 +0100

    folders: views+partials, .env

--- index.js
+++ index.js
@@ -1,2 +1,28 @@
 const mongoose = require('mongoose')
 require('./config/db.config')
+const buyRoutes = require('./routes/buy.routes')
+const buyModel = require('./models/buy.model')
+const bodyParser = require('body-parser')
+const express = require('express')
+const app = express()
+require('dotenv').config()
+const port = process.env.PORT
+
+const hbs = require('hbs')
+hbs.registerPartials(__dirname + '/views/partials')
+
+// USE
+app.use(express.static(__dirname + '/public'))
+app.use('/', buyRoutes)
+
+// SET
+app.set('view engine', 'hbs')
+
+app.set('views', __dirname + '/views')
+
+// LISTEN
+app.listen(port, () => {
+    console.log(`Example Lovro app listening at http://localhost:${port}`)
+    console.log(__dirname)
+  })
+  
--- routes/buy.routes.js
+++ routes/buy.routes.js
@@ -1,7 +1,14 @@
 const express = require('express')
 const router = express.Router()
+const buyModel = require('../models/buy.model')
 
-
+router.get('/', (req, res) => {
+    buyModel.find()
+        .then((stuff) => {
+            console.log(stuff)
+            res.render('landing.hbs', {stuff})
+        })
+})
 
 
 
